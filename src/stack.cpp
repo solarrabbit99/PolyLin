7c09406733cb6e391ca0883ef7275193c5354ab8
zhenghanlee <hanhan_1999@hotmail.com>
2024-05-22 01:43:33 +0800

    Optimize stack lin efficiency

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,6 +1,7 @@
 #include "stack.hpp"
 
 #include <algorithm>
+#include <set>
 
 using namespace polylin;
 
@@ -32,8 +33,14 @@ bool StackLin::tune(History& hist) {
 
   std::unordered_map<value_type, time_type> minResTime, maxInvTime;
   std::unordered_map<value_type, Operation> pushOp, popOp;
+  History hist2;
   for (const Operation& o : hist) {
-    if (o.value == EMPTY_VALUE) continue;
+    if (o.value == EMPTY_VALUE) {
+      Operation o2{o};
+      o2.method = Method::PEEK;
+      hist2.emplace(std::move(o2));
+      continue;
+    }
 
     if (!minResTime.count(o.value)) {
       minResTime[o.value] = o.endTime;
@@ -43,9 +50,14 @@ bool StackLin::tune(History& hist) {
       maxInvTime[o.value] = std::max(maxInvTime[o.value], o.startTime);
     }
 
-    if (o.method == Method::PUSH) pushOp.emplace(o.value, o);
-    if (o.method == Method::POP) popOp.emplace(o.value, o);
+    if (o.method == Method::PUSH)
+      pushOp.emplace(o.value, o);
+    else if (o.method == Method::POP)
+      popOp.emplace(o.value, o);
+    else
+      hist2.emplace(o);
   }
+
   for (const auto& [value, resTime] : minResTime) {
     if (!pushOp.count(value)) return false;
     Operation& valPushOp = pushOp.at(value);
@@ -53,81 +65,46 @@ bool StackLin::tune(History& hist) {
     valPushOp.endTime = resTime;
     valPopOp.startTime = maxInvTime[value];
 
-    if (valPopOp.startTime <= valPushOp.endTime) {
-      for (const Operation& o : opByVal[value]) hist.erase(o);
-      continue;
-    }
+    if (valPopOp.startTime <= valPushOp.endTime) continue;
 
     if (valPushOp.startTime >= valPushOp.endTime ||
         valPopOp.startTime >= valPopOp.endTime)
       return false;
 
-    hist.erase(valPushOp);
-    hist.erase(valPopOp);
-    hist.emplace(valPushOp);
-    hist.emplace(valPopOp);
-  }
-  return true;
-}
-
-bool StackLin::removeEmptyOps(History& hist) {
-  std::vector<std::tuple<time_type, bool, Operation>> events;
-  for (const Operation& o : hist) {
-    events.emplace_back(o.startTime, true, o);
-    events.emplace_back(o.endTime, false, o);
-  }
-  std::sort(events.begin(), events.end());
-
-  std::unordered_set<id_type> runningEmptyOp;
-  std::unordered_set<value_type> critVal, endedVal;
-
-  for (const auto& [time, isInv, op] : events) {
-    if (op.value != EMPTY_VALUE) {
-      if (isInv) {
-        if (op.method == Method::POP) {
-          critVal.erase(op.value);
-          endedVal.insert(op.value);
-        }
-      } else {
-        if (op.method == Method::PUSH && !endedVal.count(op.value))
-          critVal.insert(op.value);
-      }
-    } else {
-      if (isInv)
-        runningEmptyOp.insert(op.id);
-      else if (runningEmptyOp.count(op.id))
-        return false;
-      else
-        hist.erase(op);
-    }
-
-    if (critVal.empty()) runningEmptyOp.clear();
+    hist2.emplace(valPushOp);
+    hist2.emplace(valPopOp);
   }
 
+  hist2.emplace(0, Method::PUSH, EMPTY_VALUE, MIN_TIME, MIN_TIME + 1);
+  hist2.emplace(0, Method::POP, EMPTY_VALUE, MAX_TIME - 1, MAX_TIME);
+  std::swap(hist, hist2);
   return true;
 }
 
 bool StackLin::distVal(History& hist) {
-  if (!extend(hist) || !tune(hist) || !removeEmptyOps(hist)) return false;
+  if (!extend(hist) || !tune(hist)) return false;
 
-  std::unordered_set<value_type> removedVals;
-  std::vector<std::tuple<time_type, bool, Operation>> events;
-  for (const Operation& o : hist) {
-    events.emplace_back(o.startTime, true, o);
-    events.emplace_back(o.endTime, false, o);
-  }
-  std::sort(events.begin(), events.end());
-
+  std::set<std::tuple<time_type, bool, Operation>> events;
   std::unordered_map<value_type, size_t> opByVal;
-  for (const Operation& o : hist) ++opByVal[o.value];
+  for (const Operation& o : hist) {
+    ++opByVal[o.value];
+    events.emplace(o.startTime, true, o);
+    events.emplace(o.endTime, false, o);
+  }
 
-  while (removedVals.size() < opByVal.size()) {
+  while (opByVal.size()) {
     std::unordered_map<value_type, size_t> freeOp;
     std::unordered_map<value_type, std::unordered_set<id_type>> runningOp;
     std::unordered_set<value_type> critVal;
 
-    for (const auto& [time, isInv, op] : events) {
-      if (removedVals.count(op.value)) continue;
+    auto iter = events.begin();
+    while (iter != events.end()) {
+      const auto& [time, isInv, op] = *iter;
+
+      if (!opByVal.count(op.value)) {
+        iter = events.erase(iter);
+        continue;
+      }
 
       if (isInv) {
         runningOp[op.value].emplace(op.id);
@@ -146,13 +123,14 @@ bool StackLin::distVal(History& hist) {
         freeOp[value] += runningOp[value].size();
         runningOp.erase(value);
       }
+      ++iter;
     }
 
     bool hasRem = false;
     for (auto& [value, cnt] : freeOp)
       if (cnt == opByVal[value]) {
         hasRem = true;
-        removedVals.insert(value);
+        opByVal.erase(value);
       }
     if (!hasRem) return false;
   }
