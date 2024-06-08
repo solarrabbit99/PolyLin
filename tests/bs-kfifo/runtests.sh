60337fb6b117d8b4f3367666778f4992c7cf816c
zhenghanlee <hanhan_1999@hotmail.com>
2024-06-08 20:31:30 +0800

    Add deque testcases

---
+++ tests/bs-kfifo/runtests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+for i in {1..10};
+do
+	echo -n "Generating testcases...[$i/10]"$'\r'
+  numOp=$(( 10000 * $i ))
+	for j in {1..3};
+	do
+		./prodcon-bs-kfifo -producers=2 -consumers=2 -operations=$numOp -log_operations=1 -print_summary=0 -shuffle_threads=1 > temp.log
+		./format_history.py temp.log bs-kfifo.$i$j.log
+		rm -f temp.log
+	done
+done
+
+echo "Starting PolyLin on testcases... find dump in report.txt"
+echo filename,number of operations,time taken > report.txt
+for i in {1..10};
+do
+	echo -n "Running testcases...[$i/10]"$'\r'
+	for j in {1..3};
+	do
+	  file=bs-kfifo.$i$j.log
+		time=$({ /usr/bin/time -v ../../build/engine $file; } 2>&1 | grep "User time" | awk '{print $NF}')
+		numOp=$(wc -l $file | awk '{print $1}')
+		echo $file,$numOp,$time >> report.txt
+	done
+done
